7c2ca2577347b2ba3a85b2f4842e6874af6d421c
galibhabibullah85 <75194709+galibhabibullah85@users.noreply.github.com>
2021-05-28 18:11:52 +0600

    Add files via upload

---
+++ shopping_list.py
@@ -0,0 +1,31 @@
+shopping_list = []
+total_ammount = 0
+
+while 1:
+	item_name = input("Item name: ")
+
+	if item_name=='ext':
+		print('\n')
+		print("---------------------------------------------------------",end='\n')
+
+		for x in range(0,len(shopping_list)):
+			print(f"{shopping_list[x][0]} => {shopping_list[x][1]}",end='\n')
+			total_ammount += int(shopping_list[x][1])
+		
+		print("---------------------------------------------------------",end='\n')
+		print(f"Total ammount: {total_ammount}",end='\n')
+
+		break
+
+	item_peice = input("Item price: ")
+
+	shopping_list.append([item_name,item_peice])
+
+	print('\n')
+	print("---------------------------------------------------------",end='\n')
+
+	for x in range(0,len(shopping_list)):
+		print(f"{shopping_list[x][0]} => {shopping_list[x][1]}",end='\n')
+	
+	print("---------------------------------------------------------",end='\n')
+	print('\n')
